85e0dda0d360b41f592d0402308acbc57d790516
DavidPatranjel <davidpatranjel24@gmail.com>
2023-01-11 02:45:59 +0200

    final changes final 2

--- Taskify/wwwroot/js/filter.js
+++ Taskify/wwwroot/js/filter.js
@@ -9,17 +9,22 @@
 
 
         let user_sesiune = document.getElementById("id_user_sesiune").innerHTML.trim();
-        console.log(user_sesiune);
+        ///console.log(user_sesiune);
 
         var tasks = document.getElementsByClassName("taskuri");
+        var task_front = document.getElementsByClassName("taskuri-asoc");
+        var i = 0;
         for (let task of tasks) {
             task.style.display = "none";
+            task_front[i].style.display = "none";
             let user_task = task.getElementsByClassName("id_user_task")[0].innerHTML.trim();
-            console.log(user_task);
+            ////console.log(user_task);
             let conditie_finala = (user_sesiune == user_task);
             if (conditie_finala || !flag) {
                 task.style.display = "block";
+                task_front[i].style.display = "block";
             }
+            i++;
         }
     }
 });
